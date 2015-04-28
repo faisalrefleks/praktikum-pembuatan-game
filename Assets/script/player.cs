3c1fd84a009de4fb6a71aa0f2311a0b2b30f6e1d
faisalrefleks <60200112041@uin-alauddin.ac.id>
2015-04-29 03:02:16 +0800

    laporan1

---
+++ Assets/script/player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	Vector3 position;
+	bool jump;
+	float speedmove=60;
+	float speedjump=300;
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			position= transform.position+Vector3.left;
+			this.gameObject.transform.position = Vector3.MoveTowards (transform.position, position, speedmove * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			position= transform.position+Vector3.right;
+			this.gameObject.transform.position = Vector3.MoveTowards (transform.position, position, speedmove * Time.deltaTime);
+		}
+		if (!jump) {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				GetComponent<Rigidbody> ().velocity = Vector3.zero;
+				GetComponent<Rigidbody> ().AddForce (Vector3.up * speedjump);		
+			}
+		}
+	}
+}
